748ebd0af9b966ef4c9d2a9a639f15e0d1c596b7
YaOn999 <lumous999@gmail.com>
2022-12-30 00:22:00 +0300

    add HW_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int firstNumber, int secondNumber)
+{
+    if(firstNumber == secondNumber) return firstNumber;
+    return (firstNumber + SumNumbers(++firstNumber, secondNumber));
+}
+
+int EnterData (string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int firstNumber = EnterData("введите первое натуральное число: ");
+int secondNumber = EnterData("введите второе натуральное число: ");
+Console.WriteLine(SumNumbers(firstNumber, secondNumber));
